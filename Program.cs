3a97bfab268aa41d3348987084b90d3855d3ef99
Nolfi <sourcebans.nolfi@gmail.com>
2025-01-28 11:08:54 +0100

    Added Menu

--- Program.cs
+++ Program.cs
@@ -3,15 +3,75 @@
 namespace H2_OOP_OPG;
 
 class Program {
+    public static bool Exit = false;
+
     static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
-        using (var connection = DB.openConnection()) {
-            var query = "SELECT * FROM User";
-            using var command = new MySqlCommand(query, connection);
-            using var reader = command.ExecuteReader();
-            while (reader.Read()) {
-                Console.WriteLine(reader.GetString("Brugernavn"));
-            }
-        }
+        // Initialize the menu
+        Menu.Initialize("Sommerhus Udlejning", true, 7, 7);
+
+        // Main menu
+        do {
+            // Name of the project
+            string Title = "Home";
+
+            // Array of options to be displayed
+            string[] options = {
+                    "Login",
+                };
+
+            // Array of actions to be called
+            Action[] cases = {
+                    () => {},
+                    () => {},
+                };
+
+            // Build main menu
+            Menu.buildMain(options, cases, Title);
+
+        } while (!Exit);
+    }
+
+    public static void MainMenu() {
+        // Name of submenu
+        string name = "Main Menu";
+
+        // Options to be displayed
+        string[] options = {
+                "Sommerhus Ejere",
+                "Sommerhuse",
+                "Udlejninger",
+                "Områder",
+                "Sæson Kategorier",
+                "Inspectører",
+            };
+
+        // Array of actions to be called
+        Action[] cases = {
+                () => SommerhusEjere(),
+            };
+
+        // Build submenu
+        Menu.buildSub(name, options, cases);
+    }
+
+    public static void SommerhusEjere() {
+        // Name of submenu
+        string name = "Sommerhus Ejere";
+
+        // Options to be displayed
+        string[] options = {
+                "Opret Sommerhus Ejer",
+                "Rediger Sommerhus Ejer",
+                "Slet Sommerhus Ejer",
+                "Vis Sommerhus Ejer",
+            };
+
+        // Array of actions to be called
+        Action[] cases = {
+                () => {},
+            };
+
+        // Build submenu
+        Menu.buildSub(name, options, cases);
     }
 }
