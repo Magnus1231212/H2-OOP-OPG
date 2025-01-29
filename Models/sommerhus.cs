b12e115e419fe9af7148b872ac5db0487c91ce3b
Sebe112 <92109053+Sebe112@users.noreply.github.com>
2025-01-29 10:35:30 +0100

    Sommerhus create

--- Models/sommerhus.cs
+++ Models/sommerhus.cs
@@ -66,4 +66,36 @@ public class Sommerhus {
         connection.Close();
         return sommerhus;
     }
+
+    public static Sommerhus FindSommerhus(string location) {
+        MySqlConnection connection = DB.openConnection();
+        MySqlCommand command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Sommerhus WHERE Lokation = @location";
+        command.Parameters.AddWithValue("@location", location);
+
+        MySqlDataReader reader = command.ExecuteReader();
+        if (!reader.Read()) {
+            connection.Close();
+            return null;
+        }
+
+        Sommerhus sommerhus = Parsing.ParseSommerhus(reader);
+        connection.Close();
+        return sommerhus;
+    }
+
+    public bool Save() {
+        MySqlConnection connection = DB.openConnection();
+        MySqlCommand command = connection.CreateCommand();
+        command.CommandText = "INSERT INTO Sommerhus (EjerID, Lokation, Klassifikation, OmraadeID, StandardPris) VALUES (@ejerID, @lokation, @klassifikation, @omraadeID, @standardPris)";
+        command.Parameters.AddWithValue("@ejerID", EjerID);
+        command.Parameters.AddWithValue("@lokation", Lokation);
+        command.Parameters.AddWithValue("@klassifikation", Klassifikation);
+        command.Parameters.AddWithValue("@omraadeID", OmraadeID);
+        command.Parameters.AddWithValue("@standardPris", StandardPris);
+
+        int rows = command.ExecuteNonQuery();
+        connection.Close();
+        return rows > 0;
+    }
 }
